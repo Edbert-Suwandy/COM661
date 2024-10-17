df0178cd6124548480fb1b5b01b5e0d2164763a9
Edbert-Suwandy <edbert@edbert.>
2024-10-17 22:10:39 +0100

    upload endpoint created

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,13 +1,28 @@
-from flask import Flask, make_response, jsonify
-from os import getenv
+from flask import Flask, make_response, jsonify, request, session
+from os import getenv, path
+from werkzeug.utils import secure_filename
+
+from parse_csv import parse_csv
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = getenv("UPLOAD_FOLDER")
+app.secret_key = 'hello'
 
 # Make sure its admin locked
-@app.route('/creds', method=["GET"])
+@app.route('/env', methods=["GET"])
 def creds():
     return make_response(jsonify({
         "MONGO_USER":getenv("MONGO_USER"),
         "MONGO_PASSWORD":getenv("MONGO_PASSWORD"),
         "MONGO_URL":getenv("MONGO_URL")
     }))
+
+# Make sure its admin locked
+@app.route("/upload", methods=["POST"])
+def upload_csv():
+    f = request.files.get('file')
+    data_filename = secure_filename(f.filename)
+    f.save(path.join(app.config['UPLOAD_FOLDER'],data_filename))
+    session['uploaded_data_file_path'] = path.join(app.config['UPLOAD_FOLDER'],data_filename)
+
+    return (parse_csv("./datastore/"+data_filename))
--- backend/app/parse_csv.py
+++ backend/app/parse_csv.py
@@ -1,5 +1,23 @@
 import csv
-with open('Giants.csv', mode ='r')as file:
-  csvFile = csv.reader(file)
-  for lines in csvFile:
-        print(lines)
+import json
+
+def parse_csv(file_path: str):
+    all = []
+    with open(file_path, mode ='r')as file:
+        csvFile = csv.reader(file)
+        for lines in csvFile:
+            out = dict()
+            out = {
+                "Business Area": lines[0],
+                "Date of Offer": lines[1],
+                "Offered to": lines[2],
+                "Ultimate Recipient": lines[3],
+                "Offered From": lines[4],
+                "Description of Offer": lines[5],
+                "Reason for offer": lines[6],
+                "Details of contract": lines[7],
+                "Estimated Gift Value": lines[8],
+                "Action Taken": lines[9]
+            }
+            all.append(out)
+    return all
