39024b804bb4570766418649467ae8f2410a4449
Edbert-Suwandy <suwandy.edbert@gmail.com>
2024-10-25 00:33:00 +0100

    time filter, paginations

--- backend/app/app.py
+++ backend/app/app.py
@@ -3,6 +3,7 @@ from os import getenv, path
 from werkzeug.utils import secure_filename
 import pymongo
 from pymongo.collection import Collection
+from datetime import datetime
 
 import json
 from bson import ObjectId
@@ -14,13 +15,10 @@ app.config["UPLOAD_FOLDER"] = getenv("UPLOAD_FOLDER")
 app.secret_key = 'hello'
 
 def get_collection() -> Collection:
-    try:
-        client = pymongo.MongoClient(getenv("MONGO_URL"))
-        db = client["DoF-gratuity"]
-        collection = db["DoF"]
-        return collection
-    except:
-        return None
+    client = pymongo.MongoClient(getenv("MONGO_URL"))
+    db = client["DoF-gratuity"]
+    collection = db["DoF"]
+    return collection
 
 # TO-DO: Make sure its admin locked
 @app.route('/env', methods=["GET"])
@@ -35,6 +33,8 @@ def creds():
 @app.route("/upload", methods=["POST"])
 def upload_csv():
     f = request.files.get('file')
+    if f == None:
+        return jsonify({"message":"no file in the body of request found"})
     data_filename = secure_filename(f.filename)
     f.save(path.join(app.config['UPLOAD_FOLDER'],data_filename))
     session['uploaded_data_file_path'] = path.join(app.config['UPLOAD_FOLDER'],data_filename)
@@ -43,7 +43,6 @@ def upload_csv():
 
     collection = get_collection()
 
-    # TO-DO Maybe upsert could be cool
     for input in inputs:
         details = get_collection().find_one({"Ultimate_Recipient":input.get("Ultimate_Recipient")})
         if details != None:
@@ -51,30 +50,56 @@ def upload_csv():
         else:
             collection.insert_one(input)
 
-    # TO-DO: On success delete the csv
+    # TO-DO: On success delete the csv (Debatable on this)
     if(collection != None):
        return jsonify({"success": True})
     return jsonify({"success":False})
 
-#TO-DO: ADD PAGINATIONS
 @app.route("/member", methods=["GET"])
 def get_all():
-    result = get_collection().find({})
+    paginate:int = request.args.get(key="paginate",default=10, type=int)
+    if paginate <= 1:
+        paginate = 10
+    page:int = request.args.get(key="page",default=1,type=int)
+    if page <= 1:
+        page = 1
+
+    result = get_collection().find({}).skip((page-1)*paginate).limit(paginate)
     resp = result.to_list()
     return jsonify(json.dumps(obj=resp,cls=JSONEncoder))
 
-#TO-DO: ADD PAGINATIONS FOR GRATUITY TABLE
 @app.route("/member/<string:id>")
 def get_specific(id):
-    # return jsonify({"id":id})
     collection = get_collection()
-    resp = collection.find_one(filter={"_id":ObjectId(id)})
-    if resp != None:
-        return jsonify(json.dumps(obj=resp,cls=JSONEncoder))
-    #TO-DO: Return the acceptance rate of gratuity all time
-    #TO-DO: Gratuity offered (Accepted and denied) timeline
-    #TO-DO: Company offering gratuity timeline
-    return jsonify({"Message":"id not found"})
+
+    #TO-DO Deduping entries (TECH DIFF)
+    #TO-DO implement start and end
+    start = request.args.get(key="start",default="01/01/1000")
+    end = request.args.get(key="end",default=datetime.today().strftime('%d/%m/%Y'))
+
+    start_isotime = datetime.strptime(start, "%d/%m/%Y").isoformat()
+    end_isotime = datetime.strptime(end, "%d/%m/%Y").isoformat()
+
+    # This look hidious but lets do it for performance maybe but deffo for score :D
+    resp = collection.aggregate([{"$match":{"_id":ObjectId(id)}},{"$unwind": "$Gifts"},{"$match":{"Gifts.Date_of_Offer":{"$gt":start_isotime,"$lt":end_isotime}}},{"$project": {"Gifts":1}}]).to_list()
+
+    if resp == None:
+        return jsonify({"message":"id not found"})
+
+    # resp = collection.find_one(filter={"_id":ObjectId(id)})
+
+    # gifts = resp.get("Gifts")
+   # accepted = 0
+
+    #This is done instead of mongo query to reduce query to db
+   #  for gift in gifts:
+    #    if str(gift.get("Action_Taken")).lower() == "accepted":
+   #         accepted+=1
+
+    # resp["total_gift"] = len(gifts)
+    # resp["accepted"] = accepted
+
+    return jsonify(json.dumps(obj=resp,cls=JSONEncoder))
 
 # SOLUTION FROM https://vuyisile.com/dealing-with-the-type-error-objectid-is-not-json-serializable-error-when-working-with-mongodb/
 class JSONEncoder(json.JSONEncoder):
--- backend/app/parse_csv.py
+++ backend/app/parse_csv.py
@@ -1,31 +1,36 @@
 import csv
-import time
+from datetime import datetime 
 
 def parse_csv(file_path: str):
     all = []
     with open(file_path, mode ='r')as file:
         csvFile = csv.reader(file)
         for lines in csvFile:
+            try:
+                date = datetime.strptime(lines[1],'%d/%m/%Y').isoformat()
+            except:
+                # set default value to 00/00/0000
+                date = datetime.strptime("01/01/2000", '%d/%m/%Y').isoformat()
+
             gift_details = [{
-                "Date_of_Offer": lines[1],
+                # need error handling
+                "Date_of_Offer": date,
                 "Offered_to": lines[2],
                 "Offered_From": lines[4],
                 "Description_of_Offer": lines[5],
                 "Reason_for_offer": lines[6],
-                "Details_of_contract": lines[7],
+                "Details_of_c ontract": lines[7],
                 "Estimated_Gift_Value": lines[8],
                 "Action_Taken": lines[9],
-                "timestamp": time.time()
-            }]
-            
+                "timestamp": datetime.now().isoformat() 
+            }] 
             out = {
                 "Business_Area": lines[0],
                 "Ultimate_Recipient": get_recipient(lines[2],lines[3]),
                 "Gifts" : gift_details     
             }
-
             all.append(out)
-    return all
+        return all
 
 # We want ultimate recipent
 def get_recipient(recipient: str, ultimate_recipeint: str) -> str:
