1bfc0a6691884a203b7e9ab5235fca05e03491a6
Edbert-Suwandy <suwandy.edbert@gmail.com>
2024-10-31 01:32:13 +0000

    update member add delete and put endpoint for gifts and member

--- app/blueprints/v1/member.py
+++ app/blueprints/v1/member.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, make_response, jsonify
 from datetime import datetime
 from bson import ObjectId
 from globals import DoF, ObjectId_to_str
+from decorator import admin_required 
 
 member_bp = Blueprint("member_BP", __name__)
 
@@ -32,6 +33,11 @@ def get_specific_member(id):
     start_isotime = datetime.strptime(start, "%d/%m/%Y").isoformat()
     end_isotime = datetime.strptime(end, "%d/%m/%Y").isoformat()
 
+    try:
+        id = ObjectId(id)
+    except:
+        return make_response(jsonify({"message": id + "is not a valid id"}),400)
+
     resp = DoF.aggregate([{"$match":{"_id":ObjectId(id)}},\
             {"$unwind": "$Gifts"},\
             {"$match":{"Gifts.Date_of_Offer":{"$gt":start_isotime,"$lt":end_isotime}}},\
@@ -40,9 +46,70 @@ def get_specific_member(id):
             ])\
             .to_list()
     if resp == None:
-        return jsonify({"message":"id not found"})
+        return make_response(jsonify({"message":"id not found check url"}),400)
 
-    for e,_ in enumerate(resp):
-        resp[e]["_id"]=str(resp[e]["_id"])
-
+    resp = ObjectId_to_str(resp)
     return make_response(jsonify(resp),200)
+
+@member_bp.route("/<string:id>/delete",methods=["DELETE"])
+@admin_required
+def delete_member(id):
+    try:
+        id = ObjectId(id)
+    except:
+        return make_response(jsonify({"message": id + "is not a valid id"}),400)
+
+    resp = DoF.delete_one(filter={"_id":id})
+
+    if not resp.acknowledged:
+        return make_response(jsonify({"message": "query not acknowledged try again later"}),500)
+    
+    return make_response(jsonify({"message": "user succesfully deleted", "id": id}),200)
+
+@member_bp.route("/<string:id>/update",methods=["PATCH"])
+@admin_required
+def update_member(id):
+    try:
+        id = ObjectId(id)
+    except:
+        return make_response(jsonify({"message": id + "is not a valid id"}),400)
+
+    # ASK THIS I CAN VALIDATE INPUT BUT ALSO THE WHOLE POINT OF MONGO DB IS IT CAN MUTATE QUICKLY
+    key = request.headers.get("key")
+    if key != "Ultimate_Recipient" and key != "Business_Area":
+        return make_response(jsonify({"message": "Key must be either Business_Area or Ultimate_Recipient"}),400)
+    value = request.headers.get("value")
+    if key == None or value == None:
+        return make_response(jsonify({"message": "bad request both key and value must be filled"}, 400))
+
+    resp = DoF.update_one(filter={"_id":ObjectId(id)},update={"$set":{key:value}},upsert=True).acknowledged
+
+    if not resp:
+        return make_response(jsonify({"message": "query not acknowledged try again later"}),500)
+    return make_response(jsonify({"message": "user succesfully deleted", "id": id}),200)
+
+
+@member_bp.route("gift/<string:hash>/update",methods=["PATCH"])
+@admin_required
+def update_gift(hash):
+    key = request.headers.get("key")
+    value = request.headers.get("value")
+    if key == None or value == None:
+        return make_response(jsonify({"message": "bad request both key and value must be filled"}, 400))
+
+    resp = DoF.update_one(filter={"Gifts": {"hash": hash}}, update={"$set": {"Gifts.$."+ key: value}},upsert=True).acknowledged
+
+    if not resp:
+        return make_response(jsonify({"message": "query not acknowledged try again later"}),500)
+    
+    return make_response(jsonify({"message": "gift succesfully deleted", "id": id}),200)
+
+@member_bp.route("gift/<string:hash>/delete",methods=["DELETE"])
+@admin_required
+def delete_member(hash):
+    resp = DoF.delete_one(filter={"Gifts": {"hash": hash}})
+
+    if not resp.acknowledged:
+        return make_response(jsonify({"message": "query not acknowledged try again later"}),500)
+    
+    return make_response(jsonify({"message": "gifts succesfully deleted"}),200)
